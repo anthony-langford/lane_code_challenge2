19a492859f8375bd993554a753bc5769343e3db2
Anthony Langford <anthony.langford@gmail.com>
2017-03-24 17:07:00 -0400

    working image upload

--- src/App.js
+++ src/App.js
@@ -2,23 +2,83 @@ import React, { Component } from 'react';
 import './App.css';
 import ContactForm from './ContactForm';
 
+// class App extends Component {
+
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       uploadedFile: null,
+//       uploadedFileCloudinaryUrl: ''
+//     };
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <ContactForm className="form" />
+//         <img src={this.state.uploadedFileCloudinaryUrl} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
 class App extends Component {
 
   constructor(props) {
     super(props);
-
     this.state = {
-      uploadedFile: null,
-      uploadedFileCloudinaryUrl: ''
+      file: '',
+      imagePreviewUrl: ''
     };
-  }
+  };
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    // TODO: do something with -> this.state.file
+    console.log('handle uploading-', this.state.file);
+  };
+
+  _handleImageChange(e) {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    };
+
+    reader.readAsDataURL(file);
+  };
+
   render() {
+    let {imagePreviewUrl} = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = (<img className="imageDisplay" src={imagePreviewUrl} />);
+    }
+
     return (
-      <div className="App">
-        <ContactForm className="form" />
-        <img src={this.state.uploadedFileCloudinaryUrl} />
+      <div className="previewComponent">
+        <div className="imgPreview">
+          {$imagePreview}
+        </div>
+        <form onSubmit={(e)=>this._handleSubmit(e)}>
+          <input className="fileInput"
+            type="file"
+            onChange={(e)=>this._handleImageChange(e)} />
+          <button className="submitButton"
+            type="submit"
+            onClick={(e)=>this._handleSubmit(e)}>Upload Image</button>
+        </form>
       </div>
-    );
+    )
   }
 }
 
