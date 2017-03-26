3b590818ec0ed6b9945856846fab1bc6b3c75892
Anthony Langford <anthony.langford@gmail.com>
2017-03-26 14:33:19 -0400

    responsive design skeleton

--- src/App.js
+++ src/App.js
@@ -43,24 +43,55 @@ class App extends Component {
     };
 
     return (
-      <div className="previewComponent">
-        <div className="imgPreview">
-          {$imagePreview}
-        </div>
-        <form onSubmit={(e)=>this._handleSubmit(e)}>
-          <input className="fileInput"
-            type="file"
-            onChange={(e)=>this._handleImageChange(e)} />
-          <button className="submitButton"
+      <div id="pagewrap">
+
+        <section id="content">
+          <h2>1st Content Area</h2>
+          <p>This page demonstrates a 3 column responsive layout, complete with responsive images and jquery slideshow.</p>
+          <div className="previewComponent">
+            <div className="imgPreview">
+              {$imagePreview}
+            </div>
+            <form onSubmit={(e)=>this._handleSubmit(e)}>
+              <input className="fileInput"
+                type="file"
+                onChange={(e)=>this._handleImageChange(e)} />
+              <button className="submitButton"
+                type="submit"
+                onClick={(e)=>this._handleSubmit(e)}>Upload Image
+              </button>
+            </form>
+          </div>
+        </section>
+
+        <section id="middle">
+          <h2>2nd Content Area</h2>
+          <button className="rotateButton"
             type="submit"
-            onClick={(e)=>this._handleSubmit(e)}>Upload Image
+            onClick={(e)=>this._handleSubmit(e)}>Rotate Image
           </button>
-        </form>
-        <button className="rotateButton"
-          type="submit"
-          onClick={(e)=>this._handleSubmit(e)}>Rotate Image
-        </button>
+          <button className="rotateButton"
+            type="submit"
+            onClick={(e)=>this._handleSubmit(e)}>Rotate Image
+          </button>
+          <button className="rotateButton"
+            type="submit"
+            onClick={(e)=>this._handleSubmit(e)}>Rotate Image
+          </button>
+        </section>
+
+        <aside id="sidebar">
+          <h2>3rd Content Area</h2>
+          <p>Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+          <p>Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
+        </aside>
+
+        <footer>
+        </footer>
+
       </div>
+
     )
   }
 }
