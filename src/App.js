c5b74bc16bb835cdf0ed05b65b9542874a1b1930
Anthony Langford <anthony.langford@gmail.com>
2017-03-26 17:59:36 -0400

    added reset button and refactored

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
     };
   };
 
-  // handler functions
+  // event handlers
 
   _handleSubmit(e) {
     e.preventDefault();
@@ -78,59 +78,113 @@ class App extends Component {
     }
   };
 
+  _handleReset(e) {
+    e.preventDefault();
+    console.log('handle reset');
+    this.setState({
+      rotated: false,
+      translated: false,
+      scaled: false,
+      opacified: false
+    });
+  };
+
   render() {
     let { imagePreviewUrl } = this.state;
     let $imagePreview = null;
-    let divStyle = {};
-
-    if (this.state.rotated) {
-      divStyle["transform"] = "rotate(45deg)";
-    } else {
-      divStyle["transform"] = null;
-    }
-
-    // better way to do this for scaling? appending?
-    if (this.state.translated && this.state.scaled) {
-      divStyle["transform"] = "translate(0px,20px) scale(0.5)";
-    } else if (this.state.translated && !this.state.scaled) {
-      divStyle["transform"] = "translate(0px,20px)";
-    } else if (!this.state.translated && this.state.scaled) {
-      divStyle["transform"] = "scale(0.5)";
-    } else {
-      divStyle["transform"] = null;
-    }
-
-    if (this.state.opacified) {
-      divStyle["opacity"] = 0.5;
-    } else {
-      divStyle["opacity"] = null;
-    }
+    let imgStyle = {};
+    let transformations = [];
 
     if (imagePreviewUrl) {
-      $imagePreview = (<img className="imageDisplay" src={ imagePreviewUrl } style={ divStyle } />);
+      $imagePreview = (<img className="imageDisplay" src={ imagePreviewUrl } style={ imgStyle } />);
     } else {
       $imagePreview = (<div className="previewText"><h2>Upload an image</h2></div>);
     };
 
-    let $rotateButton = <button className="rotateButton"
-      type="submit"
-      onClick={ (e) => this._handleRotate(e) }>Rotate
-    </button>
+    // state listeners
 
-    let $translateButton = <button className="translateButton"
-      type="submit"
-      onClick={ (e) => this._handleTranslate(e) }>Translate
-    </button>
+    if (this.state.rotated) {
+      transformations.push("rotate(45deg)");
+      imgStyle["transform"] = transformations.join(" ");
+    } else {
+      let index = transformations.indexOf("rotate(45deg)");
+      if (index > -1) {
+        transformations.splice(index, 1);
+      }
+    }
 
-    let $scaleButton = <button className="scaleButton"
-      type="submit"
-      onClick={ (e) => this._handleScale(e) }>Scale
-    </button>
+    if (this.state.translated) {
+      transformations.push("translate(0px,20px)");
+      imgStyle["transform"] = transformations.join(" ");
+    } else {
+      let index = transformations.indexOf("translate(0px,20px)");
+      if (index > -1) {
+        transformations.splice(index, 1);
+      }
+    }
 
-    let $opacityButton = <button className="opacityButton"
-      type="submit"
-      onClick={ (e) => this._handleOpacity(e) }>Opacity
-    </button>
+    if (this.state.scaled) {
+      transformations.push("scale(0.5)");
+      imgStyle["transform"] = transformations.join(" ");
+    } else {
+      let index = transformations.indexOf("scale(0.5)");
+      if (index > -1) {
+        transformations.splice(index, 1);
+      }
+    }
+
+    if (this.state.opacified) {
+      imgStyle["opacity"] = 0.5;
+    } else {
+      imgStyle["opacity"] = null;
+    }
+
+    // buttons
+
+    let $rotateButton =
+    <div>
+      <button className="rotateButton"
+        type="submit"
+        onClick={ (e) => this._handleRotate(e) }>Rotate
+      </button>
+      <br/>
+    </div>
+
+    let $translateButton =
+    <div>
+      <button className="translateButton"
+        type="submit"
+        onClick={ (e) => this._handleTranslate(e) }>Translate
+      </button>
+      <br/>
+    </div>
+
+    let $scaleButton =
+    <div>
+      <button className="scaleButton"
+        type="submit"
+        onClick={ (e) => this._handleScale(e) }>Scale
+      </button>
+      <br/>
+    </div>
+
+    let $opacityButton =
+    <div>
+      <button className="opacityButton"
+        type="submit"
+        onClick={ (e) => this._handleOpacity(e) }>Opacity
+      </button>
+      <br/>
+    </div>
+
+    let $resetButton =
+    <div>
+      <button className="opacityButton"
+        type="submit"
+        onClick={ (e) => this._handleReset(e) }>Reset
+      </button>
+      <br/>
+    </div>
 
     return (
       <div id="pagewrap">
@@ -151,22 +205,17 @@ class App extends Component {
         <section id="middle">
           <h2>Available Actions</h2>
           { !this.state.rotated && $rotateButton }
-          <br/>
           { !this.state.translated && $translateButton }
-          <br/>
           { !this.state.scaled && $scaleButton }
-          <br/>
           { !this.state.opacified && $opacityButton }
+          { $resetButton }
         </section>
 
         <aside id="sidebar">
           <h2>Applied Actions</h2>
           { this.state.rotated && $rotateButton }
-          <br/>
           { this.state.translated && $translateButton }
-          <br/>
           { this.state.scaled && $scaleButton }
-          <br/>
           { this.state.opacified && $opacityButton }
         </aside>
 
